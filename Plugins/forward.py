889f1e957962129745d4ade45df59c13db7d4769
Vivek Raj Roy <83235714+unknownforall1@users.noreply.github.com>
2023-11-13 17:08:57 +0530

    Update forward.py

--- Plugins/forward.py
+++ Plugins/forward.py
@@ -12,13 +12,13 @@ from config import Config
 
 from_channel = "-1002004110207"
 to_channel = "-1002110715677"
+
 @channelforward.on_message(filters.channel)
 async def forward(client, message):
-    # Forwarding the messages to the channel
-         if message.chat.id == int(from_channel):
-            func = message.copy if Config.AS_COPY else message.forward
-            await func(int(to_channel), Config.AS_COPY)
-            logger.info("Forwarded a message from", from_channel, "to", to_channel)
-            await asyncio.sleep(1)
-   except Exception as e:
-      logger.exception(e)
+    if message.chat.id == int(from_channel):
+        func = message.copy if Config.AS_COPY else message.forward
+        await func(int(to_channel), Config.AS_COPY)
+        logger.info("Forwarded a message from", from_channel, "to", to_channel)
+        await asyncio.sleep(1)
+except Exception as e:
+logger.exception(e)
