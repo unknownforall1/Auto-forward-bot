b5a059a6569fbea532b7f1efea44a9eda1c38423
Vivek Raj Roy <83235714+unknownforall1@users.noreply.github.com>
2023-11-13 17:15:26 +0530

    Update forward.py

--- Plugins/forward.py
+++ Plugins/forward.py
@@ -19,6 +19,6 @@ async def forward(client, message):
         func = message.copy if Config.AS_COPY else message.forward
         await func(int(to_channel), Config.AS_COPY)
         logger.info("Forwarded a message from", from_channel, "to", to_channel)
-        await asyncio.sleep(1)
-except Exception as e:
-logger.exception(e)
+        await asyncio.sleep(30)
+   except Exception as e:
+      logger.exception(e)
