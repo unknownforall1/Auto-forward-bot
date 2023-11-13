4c14935a905c2f9b2896013edae674746c726b3e
Vivek Raj Roy <83235714+unknownforall1@users.noreply.github.com>
2023-11-13 22:26:23 +0530

    Update forward.py

--- Plugins/forward.py
+++ Plugins/forward.py
@@ -12,10 +12,21 @@ from config import Config
 
 @channelforward.on_message((filters.channel & filters.private) & (filters.document | filters.video ) , group=4)
 async def private_receive_handler(Client, Message):
+   try:
+      for id in Config.CHANNEL:
+         from_channel, to_channel = id.split(":")
+         if message.chat.id == int(from_channel):
+            await message.forward(chat_id=Config.BIN_CHANNEL)
+            logger.info("Forwarded a message from", from_channel, "to", to_channel)
+            await asyncio.sleep(30)
+   except Exception as e:
+      logger.exception(e)
     await message.forward(chat_id=Config.BIN_CHANNEL)
     logger.info("Forwarded a message from", from_channel, "to", to_channel)
     await asyncio.sleep(30)
 except Exception as e:
-   logger.exception(e)
+logger.exception(e)
+
+
 
 
