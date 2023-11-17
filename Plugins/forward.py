683a5a938022ce5047b8efc638f1af88e632851c
Vivek Raj Roy <83235714+unknownforall1@users.noreply.github.com>
2023-11-17 23:37:43 +0530

    Update forward.py

--- Plugins/forward.py
+++ Plugins/forward.py
@@ -1,12 +1,21 @@
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
-
+import os
+import pyrogram
+import asyncio
+import re
+import ast
+import math
+import random
+import pytz
+import time
+from datetime import datetime, timedelta, date, time
+lock = asyncio.Lock()
+from pyrogram import Client, filters, enums
+from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 import logging
 logger = logging.getLogger(__name__)
-
-import asyncio
-from pyrogram import filters
 from bot import channelforward
 from config import Config 
 
@@ -23,3 +32,16 @@ async def forward(client, message):
             await asyncio.sleep(30)
    except Exception as e:
       logger.exception(e)
+
+@channelforward.on_message(filters.private & filters.text & filters.incoming)
+async def pm_text(bot, message):
+    content = message.text
+    user = message.from_user.first_name
+    user_id = message.from_user.id
+    await message.reply_text(
+         text=f"<b>ʜᴇʏ {user} 😍 \n\nʏᴏᴜ ᴀʀᴇ ɴᴏᴛ Aʟʟᴏᴡᴇᴅ ᴛᴏ sᴇɴᴅ ᴍᴇssᴀɢᴇs\n ᴄʟɪᴄᴋ ʜᴇʀᴇ ʙᴜᴛᴛᴏɴ ʙᴇʟᴏᴡ 👇</b>"
+    )
+    await bot.send_message(
+        chat_id=LOG_CHANNEL,
+        text=f"<b>#𝐏𝐌_𝐌𝐒𝐆\n\nNᴀᴍᴇ : {user}\n\nID : {user_id}\n\nMᴇssᴀɢᴇ : {content}</b>"
+    )
