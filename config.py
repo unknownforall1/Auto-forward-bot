9d6980c5b8be6f04d0e0a07d7d2f12f33be079c8
Vivek Raj Roy <83235714+unknownforall1@users.noreply.github.com>
2023-11-17 23:45:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
       AS_COPY = True if getenv("AS_COPY", True) == "`{file_name}`" else True
       BOT_TOKEN = getenv("BOT_TOKEN", "6730235175:AAHpaDLbk4_wpSCePevYF_XsWkbDB-LMjCk")
       session_string = getenv("session_string", "")
-      BIN_CHANNEL = getenv("BIN_CHANNEL", "")
+      LOG_CHANNEL = getenv("BIN_CHANNEL", "")
       CHANNEL = list(x for x in getenv("CHANNEL_ID", "-1002000077230:-1002083341321").replace("\n", " ").split(' '))
 
 
