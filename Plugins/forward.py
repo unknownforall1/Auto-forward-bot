91da6240154e22fd1b9025637e9667253dc818eb
Vivek Raj Roy <83235714+unknownforall1@users.noreply.github.com>
2023-11-17 23:20:25 +0530

    Add files via upload

--- Plugins/forward.py
+++ Plugins/forward.py
@@ -8,15 +8,17 @@ logger = logging.getLogger(__name__)
 import asyncio
 from pyrogram import filters
 from bot import channelforward
-from config import Config
+from config import Config 
 
-@channelforward.on_message((filters.channel & filters.private) & (filters.document | filters.video ) , group=4)
-async def private_receive_handler(Client, Message):
+@channelforward.on_message(filters.channel & (filters.document | filters.video | filters.photo)  & ~filters.forwarded)
+async def forward(client, message):
+    # Forwarding the messages to the channel
    try:
       for id in Config.CHANNEL:
          from_channel, to_channel = id.split(":")
          if message.chat.id == int(from_channel):
-            await message.forward(chat_id=Config.BIN_CHANNEL)
+            func = message.copy if Config.AS_COPY else message.forward
+            await func(int(to_channel), Config.AS_COPY)
             logger.info("Forwarded a message from", from_channel, "to", to_channel)
             await asyncio.sleep(30)
    except Exception as e:
