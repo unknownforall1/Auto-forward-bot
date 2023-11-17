020bb7c3928e1ae397801d0c3b12244cf2617aca
Vivek Raj Roy <83235714+unknownforall1@users.noreply.github.com>
2023-11-18 01:22:11 +0530

    Update forward.py

--- Plugins/forward.py
+++ Plugins/forward.py
@@ -41,21 +41,15 @@ async def forward(client, message):
       for id in Config.CHANNEL:
          from_channel, to_channel = id.split(":")
          if message.chat.id == int(from_channel):
-            func = message.copy if Config.AS_COPY else message.forward
-            await func(int(to_channel), Config.AS_COPY)
-            await asyncio.sleep(5)
-            logger.info("Forwarded a message from", from_channel, "to", to_channel)
-            await asyncio.sleep(30)
+             global last_message_time
+             current_time = time.time()
+             if current_time - last_message_time < 5:
+                 func = message.copy if Config.AS_COPY else message.forward
+                 await func(int(to_channel), Config.AS_COPY)
+                 await time.sleep(5)
+                 last_message_time = time.time()
+                 logger.info("Forwarded a message from", from_channel, "to", to_channel)
+                 await asyncio.sleep(30)
    except Exception as e:
-      logger.exception(e)
+       logger.exception(e)
 
-
-
-@channelforward.on_message(filters.channel & ~filters.forwarded)
-def forward_message(client, message):
-    global last_message_time
-    current_time = time.time()
-    if current_time - last_message_time < 5:
-        await time.sleep(5)
-        await message.forward(chat_id=Config.LOG_CHANNEL)
-        last_message_time = time.time()
